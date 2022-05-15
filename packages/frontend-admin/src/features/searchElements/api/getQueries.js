5764994248a593a6a4c91f2f37a377d218ee0dd3
Jakub Michnowicz <jakub.michnowicz@gmail.com>
2022-05-15 19:08:12 +0200

    ci test

--- packages/frontend-admin/src/features/searchElements/api/getQueries.js
+++ packages/frontend-admin/src/features/searchElements/api/getQueries.js
@@ -1,5 +1,7 @@
 const getQueries = async () => {
     return new Promise((resolve) => {
+        console.log('REACT_APP_API_URL', process.env.REACT_APP_API_URL);
+
         fetch(`${process.env.REACT_APP_API_URL}/v1/queries`)
             .then(response => response.json())
         .then(data => {
